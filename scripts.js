d02536ccc58c3929ca112d58ab283af020b45cd8
Andre Luiz <andredeaquino86@gmail.com>
2024-11-11 13:40:36 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+
+    const html= document.Element
+html.classList.toggle('light')
+  
+const img =querySelector('#profile img')
+
+if (classList.contains('light')) {
+
+    img.setAtrribute('src', './assets/Avatar-light.png')
+}
+else {
+
+    img.setAtrribute('src', './assets/Avatar.png')
+}
+      
+}
